c358fa0b092a1ff9a5c16299f5e9a630e682d8da
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2023-12-22 23:26:56 +0530

    project setup done!

---
+++ ChatSync/src/main/java/com/chatsync/app/ChatSyncApplication.java
@@ -0,0 +1,13 @@
+package com.chatsync.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSyncApplication.class, args);
+	}
+
+}
---
+++ ChatSync/src/main/java/com/chatsync/app/models/Message.java
@@ -0,0 +1,7 @@
+package com.chatsync.app.models;
+
+public class Message {
+
+    private String name;
+    private String content;
+}
