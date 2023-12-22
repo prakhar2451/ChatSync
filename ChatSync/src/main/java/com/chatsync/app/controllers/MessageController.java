8069c48d4c7e37dd280e1c63228382f413b7b8ed
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2023-12-23 04:18:24 +0530

    added notice display about chat left

--- ChatSync/src/main/java/com/chatsync/app/controllers/MessageController.java
+++ ChatSync/src/main/java/com/chatsync/app/controllers/MessageController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+
     @MessageMapping("/message")
     @SendTo("/topic/return-to")
     public Message getContent (@RequestBody Message message) {
         return  message;
+    }
+
+    @MessageMapping("/leave")
+    @SendTo("/topic/return-to")
+    public  Message leaveChat() {
+        Message leaveMessage = new Message("User", " has left the chat", "leave");
+        return leaveMessage;
     }
 }
